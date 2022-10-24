a1a911f484df64448792baabeecd2f9a65c5f7b2
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-24 16:15:35 +0200

    Add ejs

--- app.js
+++ app.js
@@ -3,10 +3,60 @@ const app= express();
 const bodyParser = require("body-parser");
 //const request= require("request");
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended:true}));
 
 app.get("/",function(req,res){
-  
+    
+    var today= new Date();
+    var currentDay= today.getDay();
+    var day = "";
+    
+    switch (currentDay) {
+        case  1:
+            day="Monday";
+            break;
+    }
+    
+    switch (currentDay) {
+        case  2:
+            day="Tuesday";
+            break;
+    }
+    
+    switch (currentDay) {
+        case  3:
+            day="Wednesday";
+            break;
+    }
+    
+    switch (currentDay) {
+        case  4:
+            day="Thursday";
+            break;
+    }
+    
+    switch (currentDay) {
+        case  5:
+            day="Friday";
+            break;
+    }
+    
+    switch (currentDay) {
+        case  6:
+            day="Saturday";
+            break;
+    }
+    
+    switch (currentDay) {
+        case  0:
+            day="Sunday";
+            break;
+            default:
+        console.log("Error: Current day is equal to:"+ currentDay)
+        }
+
+    res.render("list", { kindOfDay: day});
 });
 
 app.listen(3000,function(){
