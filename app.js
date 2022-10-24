d2127cd4284e8378fd197052ca111bba19357b40
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-24 16:20:17 +0200

    ejs templeting

--- app.js
+++ app.js
@@ -12,49 +12,39 @@ app.get("/",function(req,res){
     var currentDay= today.getDay();
     var day = "";
     
-    switch (currentDay) {
-        case  1:
-            day="Monday";
-            break;
-    }
-    
-    switch (currentDay) {
-        case  2:
-            day="Tuesday";
-            break;
-    }
-    
-    switch (currentDay) {
-        case  3:
-            day="Wednesday";
-            break;
-    }
-    
-    switch (currentDay) {
-        case  4:
-            day="Thursday";
-            break;
-    }
-    
-    switch (currentDay) {
-        case  5:
-            day="Friday";
-            break;
-    }
-    
-    switch (currentDay) {
-        case  6:
-            day="Saturday";
-            break;
-    }
-    
+
     switch (currentDay) {
         case  0:
             day="Sunday";
             break;
-            default:
-        console.log("Error: Current day is equal to:"+ currentDay)
-        }
+
+            case  1:
+                day="Monday";
+                break; 
+
+                case  2:
+                    day="Tuesday";
+                    break; 
+                    
+                    case  3:
+                        day="Wednesday";
+                        break;
+
+                        case  4:
+                            day="Thursday";
+                            break;
+
+                            case  5:
+                                day="Friday";
+                                break;
+
+                                case  6:
+                                    day="Saturday";
+                                    break;
+                                    default:
+                                    console.log("Error: Current day is equal to:"+ currentDay);
+    }   
+
 
     res.render("list", { kindOfDay: day});
 });
