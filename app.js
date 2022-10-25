957797e6dd49272437fd7c7b4a8c5982f8be3780
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-25 17:05:57 +0200

    Passing data from website to server

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app= express();
 const bodyParser = require("body-parser");
 //const request= require("request");
 
+var items=[  " Grocery shopping","Buy Fruits","Cook Rice", "Curry" ];
+
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.urlencoded({ extended:true})); //must write between set& get
 
 app.get("/",function(req,res){
     
@@ -16,10 +18,16 @@ app.get("/",function(req,res){
     var today= new Date();
     var day = today.toLocaleDateString("en-US",options); //formated date pasted in below   
 
-    res.render("list", { 
-        nameOfDay: day
-    });
+    res.render("list", { nameOfDay: day,newListItems: items });
 });
+
+
+app.post("/", function(req,res){  //compilation hundler that redirects the user to the home route
+ var item = req.body.newItem;
+ items.push(item);
+res.redirect("/");
+});
+
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
