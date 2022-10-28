8700d7a4d7667097eb6e6303bd82c71ee10f5e9e
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-28 23:03:10 +0200

    add a worklist template

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
-const express= require("express");
-const app= express();
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 //const request= require("request");
 
-let items=[  " Grocery shopping","Buy Fruits","Cook Rice", "Curry" ];
-
+let items = [" Grocery shopping", "Buy Fruits", "Cook Rice", "Curry"];
+let workItems = [];
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended:true})); //must write between set& get
+app.use(bodyParser.urlencoded({ extended: true })); //must write between set& get
 app.use(express.static("public"));
 
 
-app.get("/",function(req,res){
-    
-   let options={
+app.get("/", function (req, res) {
+
+    let options = {
         weekday: "long",
-        day : "numeric",
-        month : "long"
+        day: "numeric",
+        month: "long"
     };
-    let today= new Date();
-    let day = today.toLocaleDateString("en-US",options); //formated date pasted in below   
+    let today = new Date();
+    let day = today.toLocaleDateString("en-US", options); //formated date pasted in below   
 
-    res.render("list", { nameOfDay: day,newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items });
 });
 
 
-app.post("/", function(req,res){  //compilation hundler that redirects the user to the home route
+app.post("/", function (req, res) {  //compilation hundler that redirects the user to the home route
+
     let item = req.body.newItem;
- items.push(item);
-res.redirect("/");
+
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+       
+    }
+    else {
+    
+        items.push(item);
+        res.redirect("/");
+       
+    }
 });
 
 
-app.listen(3000,function(){
+app.get("/work", function (req, res) {
+    console.log(workItems)
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+   
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
