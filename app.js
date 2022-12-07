e7bd4705cd633b772bb2f8755999c9ecf9febc02
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-12-07 21:45:25 +0100

    Adding New Items to our ToDoList Database

--- app.js
+++ app.js
@@ -64,26 +64,16 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {  //compilation hundler that redirects the user to the home route
 
-    let item = req.body.newItem;
-
-    if (req.body.list === "Work List") {
-        workItems.push(item);
-        res.redirect("/work");
        
-    }
+     //singular  collection name
+    const itemName = req.body.newItem;
+  
+    const item = new Item({  //new document
+        name:itemName
+      });
+    item.save();
+    res.redirect("/");
 
-    else if(req.body.list === "Travel Bucket List"){
-        travelItems.push(item);
-        res.redirect("/travel");
-    }
-
-
-    else {
-    
-        items.push(item);
-        res.redirect("/");
-       
-    }
 });
 
 
