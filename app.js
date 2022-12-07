f023687f169f2a1a64e9f0af4348efe8d0627a14
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-12-07 12:39:44 +0100

    Create a mongoose Model

--- app.js
+++ app.js
@@ -18,19 +18,7 @@ const itemsSchema= mongoose.Schema({
    //Schema model
    
    const Item = mongoose.model("Item",itemsSchema); //singular  collection name
-   const item1 = new Item({
-     name:"Welcome to your TO-Do-List!"
-   });
 
-   const item2 = new Item({
-    name:"Hit the + button to add a new item"
-  });
-
-  const item3 = new Item({
-    name:"<-- Hit this to delete an item."
-  });
-  
-  const defaultItems=[ item1,item2,item3];
 
 app.get("/", function (req, res) {
 
